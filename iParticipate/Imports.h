47d4d5bf9956876b4b8b24ef250aba5ae6d645fa
Max Kramer <max@maxkramer.co>
2013-11-16 14:44:56 +0000

    Initial Commit

---
+++ iParticipate/Imports.h
@@ -0,0 +1,7 @@
+//
+//  Imports.h
+//  iParticipate
+//
+//  Created by Max Kramer on 16/11/2013.
+//  Copyright (c) 2013 Max Kramer. All rights reserved.
+//
---
+++ iParticipate/iParticipate/IPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IPAppDelegate.h
+//  iParticipate
+//
+//  Created by Max Kramer on 16/11/2013.
+//  Copyright (c) 2013 Max Kramer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
